4333edefa22babfb19bdad7e7195b3908f36cf6f
Tomasz Miśkowicz <tomasz.miskowicz@swmansion.com>
2024-04-10 13:46:23 +0200

    Add constants related to RPC URLs

--- crates/shared/src/consts.rs
+++ crates/shared/src/consts.rs
@@ -1,1 +1,10 @@
+use crate::test_utils::{read_rpc_url, read_rpc_url_with_no_version};
+use lazy_static::lazy_static;
+use url::Url;
+
 pub const EXPECTED_RPC_VERSION: &str = "0.7.0";
+
+lazy_static! {
+    pub static ref RPC_URL: Url = read_rpc_url().unwrap();
+    pub static ref RPC_URL_WITH_NO_VERSION: Url = read_rpc_url_with_no_version().unwrap();
+}
--- crates/shared/src/test_utils/mod.rs
+++ crates/shared/src/test_utils/mod.rs
@@ -1,1 +1,46 @@
+use anyhow::Result;
+use anyhow::{anyhow, Context};
+use indoc::indoc;
+use std::env;
+use url::Url;
+
 pub mod output_assert;
+
+pub(crate) fn read_rpc_url() -> Result<Url> {
+    let rpc_url = env::var("RPC_URL")
+        .with_context(|| indoc! (
+            r#"
+            The required environmental variable `RPC_URL` is not set. Please set it manually or in .cargo/config.toml file:
+
+            [env]
+            RPC_URL = https://example.com/rpc/v0_7
+            "#
+        )
+    )?;
+
+    Url::parse(&rpc_url).with_context(|| {
+        format!(
+            "Failed to parse the URL from the `RPC_URL` environmental variable: {}",
+            rpc_url
+        )
+    })
+}
+
+pub(crate) fn read_rpc_url_with_no_version() -> Result<Url> {
+    let mut url = read_rpc_url().unwrap();
+    match url.path_segments_mut() {
+        Ok(mut path) => {
+            path.clear();
+        }
+        Err(_) => {
+            // TODO maybe add here `", got {}", &url`
+            return Err(anyhow!(
+                "Make sure that `RPC_URL` environmental variable is a valid node address"
+            ));
+        }
+    }
+
+    url.set_query(None);
+
+    Ok(url)
+}
